b59293882b6ef5fb0dcb7d05efcea8f10c037a70
Tiong Lee <LLLTTH@gmail.com>
2022-12-22 22:52:03 +0800

    added code with a loop to create 16x16 rows and collumn

--- etchsketch.js
+++ etchsketch.js
@@ -2,10 +2,17 @@ console.log('test JS link!');
 
 //created a variable to select the div to be appended
 const container = document.querySelector('#container');
-//created a div to hold the etch a sketch
-const content = document.createElement('div');
-//content.classList.add('content');
-//content.textContent = "Hey I'm red!";
-
-//append all into the main div container
-container.appendChild(content);
+//created a loop that creates 16 div containers
+for (let i = 0; i < 16; i++) { // outer loop to create the rows
+    const row = document.createElement('div'); // create a new div element for the row
+    row.classList.add('row'); // add a class to the row for styling purposes
+    
+    for (let j = 0; j < 16; j++) { // inner loop to create the columns
+      const col = document.createElement('div'); // create a new div element for the column
+      col.classList.add('col'); // add a class to the column for styling purposes
+      row.appendChild(col); // append the column to the row
+    }
+    
+    container.appendChild(row); // append the row to the container
+  }
+// use CSS to style the div classes of row and col
